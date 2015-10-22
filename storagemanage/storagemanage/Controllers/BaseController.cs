3089612a47debc54f14b34efd086d5117a1f4dd6
donghaowxr <610389473@qq.com>
2015-10-22 19:27:25 +0800

    添加登录页面

---
+++ storagemanage/storagemanage/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace storagemanage.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            if (Session["username"] == null)
+            {
+                filterContext.HttpContext.Response.Redirect("/Login/Login");
+            }
+        }
+    }
+}
---
+++ storagemanage/storagemanage/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace storagemanage.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+         * 登录action
+         */
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
