6e6c704a9dbe2d4710ec805bc0b35fb69edd2479
donghaowxr <610389473@qq.com>
2015-10-26 13:32:30 +0800

    修改api测试action，提交参数至api放到控制器中

--- storagemanage/storagemanage/Controllers/LoginController.cs
+++ storagemanage/storagemanage/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HttpCommand;
 
 namespace storagemanage.Controllers
 {
@@ -35,9 +36,22 @@ namespace storagemanage.Controllers
             return Json(result, JsonRequestBehavior.AllowGet);
         }
 
+        /*
+         * api测试页面
+         */
         public ActionResult Test()
         {
             return View();
+        }
+
+        /*
+         * api测试
+         */
+        public ActionResult TestPost()
+        {
+            string res = HttpPost.httppost("pos=1", "http://192.168.0.93:1000/api/Login");
+            var result = new { res = res };
+            return Json(result, JsonRequestBehavior.AllowGet);
         }
     }
 }
